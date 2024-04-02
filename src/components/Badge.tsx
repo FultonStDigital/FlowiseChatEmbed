d41453b0e86a67abd76539d561f5ad5c916691f7
Tory Barber <torybarber@Torys-MacBook-Pro.local>
2024-04-02 15:36:11 -0600

    Updated Power By Text and link

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -47,14 +47,14 @@ export const Badge = (props: Props) => {
       Powered by
       <a
         ref={liteBadge}
-        href={'https://flowiseai.com'}
+        href={'https://fultonst.digital/'}
         target="_blank"
         rel="noopener noreferrer"
         class="lite-badge"
         id="lite-badge"
         style={{ 'font-weight': 'bold', color: props.poweredByTextColor ?? defaultTextColor }}
       >
-        <span> Flowise</span>
+        <span> Fulton St. Digital</span>
       </a>
     </span>
   );
